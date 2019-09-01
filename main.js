4a9791c67c3b0ccab93d974701bad29d5c35a6fd
Slavi-Svec <slavi.svec@gmail.com>
2019-09-02 07:15:19 +1000

    start of game interface,game buttons,demo animation and clock feature

--- main.js
+++ main.js
@@ -1,1 +1,73 @@
-alert('helloWorld');
+// alert('hellosammy');
+
+
+
+var timer =window.setInterval(moveSammy,250);
+function moveSammy() {
+    document.getElementById("sammy").style.center=Math.random()*200+"px";
+    document.getElementById("sammy").style.top=Math.random()*200+"px";
+    // document.getElementById("sammy").style.left=Math.random()*500+"px";
+    // document.getElementById("sammy").style.right=Math.random()*500+"px";
+}
+
+// this is all the code for the clock feature in the console face
+function updateTime() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10){
+        seconds ='0' + seconds;
+    }
+    if (hours >= 12 && hours <24){
+        var timeOfDay = 'pm';
+    }
+    else {
+        var timeOfDay = 'am';
+    }
+    if(hours>12) {
+        hours = hours -12;
+    }
+
+    var currentTime = hours + ':' + minutes + ':' + seconds +'' + timeOfDay
+    var theClock= document.getElementById("clock-face");
+    theClock.innerHTML = currentTime;
+}
+
+function toggleClock() {
+    var theClock = document.getElementById("clock-face");
+    var displaySettings = theClock.style.display;
+    var clockButton =document.getElementById('clock-button');
+    if (displaySettings == 'block') {
+        theClock.style.display = 'none';
+        clockButton.innerHTML = 'clock';
+    }
+    else {
+    theClock.style.display = 'block';
+    clockButton.innerHTML = 'clock';
+    }
+}
+    // if (displaySettings == 'block') {
+    //     theClock.style.display = 'block';
+    //      clockButton.innerHTML = 'clock';
+    //  }
+    //  else {
+    // theClock.style.display = 'none';
+    // clockButton.innerHTML = 'clock';
+    //  }
+    // }
+
+
+
+
+    // console.log(hours,minutes)
+    // console.log(now,hours,minutes,seconds)
+// }
+// var now = new Date();
+// console.log(now)
+
+
---
+++ testcode.js
@@ -0,0 +1,62 @@
+  
+
+// alert('testcode');
+
+// // this is all the code for the clock feature in the console face
+// const clock = document.querySelector('.clock');
+
+// const tick = () => {
+
+//     const  now = new Date();
+
+//     const h = now.getHours();
+//     const m = now.getMinutes();
+//     const s = now.getSeconds();
+//     // console.log(h,m,s);
+
+//     const html =  `
+//     <span>${h}</span>
+//     <span>${m}</span>
+//     <span>${s}</span>
+//     `;
+
+//     clock.innerHTML=  html;
+
+
+// };
+
+
+// setInterval(tick, 1000);
+
+
+// function updateTime() {
+
+    // get all parts of the current time
+//   var now = new Date();
+//   var hours = now.getHours();
+//   var minutes = now.getMinutes();
+//   var seconds = now.getSeconds();
+
+//   // splice them together into a character string named "currentTime"
+//   var currentTime = hours + ':' + minutes + ':' + seconds;
+
+//   // get the clock div
+//   var myClock = document.getElementById('clock');
+
+//   // write the currentTime string to the clock div
+//   myClock.innerHTML = currentTime;
+// // }
+// var now = new Date();
+// console.log(now)
+
+// function updateTime() {
+//     var now = new Date();
+//     var hours = now.getHours();
+//     var minutes = now.getMinutes();
+//     var seconds = now.getSeconds();
+
+//     // i splice them together into a string named "currentTime"
+//     var currentTime = hours + '.' + minutes + '.' + seconds;
+//     var theClock= document.getElementById("clock-face");
+//     theClock.innerHTML = currentTime;
+// }
